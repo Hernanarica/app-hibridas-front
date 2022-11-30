2cfb8ac31225ac42dd216bd21f8402e5c21ae7d3
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-11-29 22:23:51 -0300

    se discrimina entre usuario normal, usuario registrado y admin

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,23 @@
+import { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routesPathTypes } from '../types/routesPathTypes';
+import { UserContext } from '../state/context';
 import { Home, LayoutPublic, Login, Notes, Register } from '../views';
 
 export function AppRouter() {
+  const { state } = useContext(UserContext);
+  
+  if (state.role === 'admin') {
+    console.log('Eres admin 😎');
+    return <h1>Admin role</h1>
+  }
+  
+  if (state.role === 'user') {
+    console.log('Eres user 🤔');
+    return <h1>User role</h1>
+  }
+  
+  console.log('Eres un visitante 😏');
   return (
     <>
       <Routes>
