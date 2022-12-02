6d97ca21108246521492983593b4a2fbf71ea5d6
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-12-02 14:25:19 -0300

    se mejora la ui del dashboard en el sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,44 @@
 import { Link } from 'react-router-dom';
+import { ArrowUturnLeftIcon, BookOpenIcon, Squares2X2Icon, UserGroupIcon } from '@heroicons/react/24/outline/index.js';
 
 export function Sidebar() {
 	return (
-		<aside>
-			<div>
-				<ul>
-					<li>
-						<Link to="/dashboard">Dashboard</Link>
+		<aside className="p-2 flex flex-col justify-between h-full">
+			<div className="">
+				<ul className="space-y-3">
+					<li className="group">
+						<Link to="/dashboard" className="p-2 inline-flex w-full gap-2 items-center text-gray-600 group-hover:text-gray-900">
+							<Squares2X2Icon className="h-5 w-5" />
+							<span>
+								Dashboard
+							</span>
+						</Link>
 					</li>
-					<li>
-						<Link to="usuarios">Usuarios</Link>
+					<li className="group">
+						<Link to="usuarios" className="p-2 inline-flex w-full gap-2 items-center text-gray-600 group-hover:text-gray-900">
+							<UserGroupIcon className="h-5 w-5" />
+							<span>
+								Usuarios
+							</span>
+						</Link>
 					</li>
-					<li>
-						<Link to="posts">Posts</Link>
+					<li className="group">
+						<Link to="posts" className="p-2 inline-flex w-full gap-2 items-center text-gray-600 group-hover:text-gray-900">
+							<BookOpenIcon className="h-5 w-5" />
+							<span>
+								Posts
+							</span>
+						</Link>
 					</li>
 				</ul>
 			</div>
-			<div>
-				<Link to="/">Volver al Blog</Link>
+			<div className="group">
+				<Link to="/" className="p-2 inline-flex w-full gap-2 items-center text-gray-600 group-hover:text-gray-900">
+					<ArrowUturnLeftIcon className="h-5 w-5" />
+					<span>
+						Volver al blog
+					</span>
+				</Link>
 			</div>
 		</aside>
 	);
--- src/views/Layouts/LayoutDashboard.jsx
+++ src/views/Layouts/LayoutDashboard.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 
 export function LayoutDashboard() {
 	return (
-		<div className="w-full min-h-screen border-4 border-red-600 flex">
-			<div className="w-3/12 border-2 border-blue-600">
+		<div className="w-full min-h-screen flex">
+			<div className="w-3/12">
 				<Sidebar />
 			</div>
 			<div className="w-9/12">
