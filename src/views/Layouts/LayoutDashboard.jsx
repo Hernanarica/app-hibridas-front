a66b9b4a10679593fdf384a534f88d7d16d407b9
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-12-04 16:31:31 -0300

    fix: se agrega un scroll en la seccion posts para la tabla

--- src/views/Layouts/LayoutDashboard.jsx
+++ src/views/Layouts/LayoutDashboard.jsx
@@ -3,11 +3,11 @@ import { Outlet } from 'react-router-dom';
 
 export function LayoutDashboard() {
 	return (
-		<div className="w-full min-h-screen flex">
+		<div className="w-full h-screen flex">
 			<div className="w-3/12">
 				<Sidebar />
 			</div>
-			<div className="w-9/12">
+			<div className="w-9/12 overflow-y-auto">
 				<Outlet />
 			</div>
 		</div>
