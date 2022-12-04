5556266c0ad4e09ea61616f9ed27e41fa86ef0b4
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-12-04 18:55:37 -0300

    se quita el boton de agregar usuario

--- src/views/Dashboard/Users.jsx
+++ src/views/Dashboard/Users.jsx
@@ -41,15 +41,6 @@ export function Users() {
 					<h1 className="text-xl font-semibold text-gray-900">Panel de usuarios</h1>
 					<p className="mt-2 text-sm text-gray-700">Desde este panel podrás administrar todos los usuarios del blog</p>
 				</div>
-				<div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-					<Link
-						to="crear-usuario"
-						className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-					>
-						<PlusIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-						Agregar
-					</Link>
-				</div>
 			</div>
 			<div className="mt-8 flex flex-col">
 				<div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
