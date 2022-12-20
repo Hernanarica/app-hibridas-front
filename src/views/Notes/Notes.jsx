a594a60012ef25594720e54fe0942972c53bab2c
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-12-20 12:47:57 -0300

    se borrar el ordenamiento de posts desde el front

--- src/views/Notes/Notes.jsx
+++ src/views/Notes/Notes.jsx
@@ -50,7 +50,7 @@ export function Notes() {
                   </Link>
                 </div>
               </div>
-            )).reverse()
+            ))
           }
         </div>
       </div>
